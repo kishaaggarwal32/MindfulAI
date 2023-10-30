21b652e32f4551f7bc3f51b53a421af21f13b230
ankuraxz <34418414+Ankuraxz@users.noreply.github.com>
2023-10-29 23:53:51 -0400

    Chat Beautify

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
-import { Box, Flex, Avatar, Text, Input, Button } from "@chakra-ui/react";
-import axios from "axios"; // Import axios
+import { Box, Flex, Avatar, Text, Input, Button, Spacer } from "@chakra-ui/react";
+import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
 
 const Chat = () => {
@@ -9,17 +9,17 @@ const Chat = () => {
   const [message, setMessage] = useState("");
   const [chats, setChats] = useState([]);
 
-  const sendMessage = async (stop) => {
+  const sendMessage = async (msg) => {
     try {
       let newChats = [...chats];
       newChats.push({
-        customer: stop ? "stop" : message,
+        customer: msg,
         agent: "AI is thinking ...",
       });
       setChats(newChats);
 
       const requestBody = new URLSearchParams();
-      requestBody.append("message", stop ? "stop" : message);
+      requestBody.append("message", msg);
       requestBody.append("history", JSON.stringify(history));
 
       const config = {
@@ -32,9 +32,9 @@ const Chat = () => {
 
       // Make a POST request to your chatbot service
       const response = await axios.post(
-        "https://hackathon-mindfulai.azurewebsites.net/ai/chat",
-        requestBody,
-        config
+          "https://hackathon-mindfulai.azurewebsites.net/ai/chat",
+          requestBody,
+          config
       );
 
       newChats[newChats.length - 1].agent = response.data.response;
@@ -47,53 +47,60 @@ const Chat = () => {
   };
 
   return (
-    <Box backgroundColor="white">
-      <Box
-        height="75vh"
-        maxHeight="75vh"
-        scrollBehavior="smooth"
-        overflow="scroll"
-        padding="20px"
-      >
-        {chats.map((chat, index) => (
-          <Box key={index}>
-            <Flex marginBottom="20px">
-              <Avatar
-                size="sm"
-                name="User Avatar"
-                src="https://cdn-icons-png.flaticon.com/512/6596/6596121.png"
-                marginRight="10px"
-              />
-              <Text as="span" fontSize="md">
-                {chat.customer}
-              </Text>
-            </Flex>
-            <Flex marginBottom="20px">
-              <Avatar
-                size="sm"
-                name="Bot Avatar"
-                src="https://static.vecteezy.com/system/resources/previews/010/054/157/original/chat-bot-robot-avatar-in-circle-round-shape-isolated-on-white-background-stock-illustration-ai-technology-futuristic-helper-communication-conversation-concept-in-flat-style-vector.jpg"
-                marginRight="10px"
-              />
-              <Text as="span"> {chat.agent}</Text>
-            </Flex>
-          </Box>
-        ))}
+      <Box backgroundColor="white">
+        <Box
+            height="75vh"
+            maxHeight="75vh"
+            scrollBehavior="smooth"
+            overflow="scroll"
+            padding="20px"
+        >
+          {chats.map((chat, index) => (
+              <Box key={index}>
+                <Flex marginBottom="20px">
+                  <Avatar
+                      size="sm"
+                      name="User Avatar"
+                      src="https://cdn-icons-png.flaticon.com/512/6596/6596121.png"
+                      marginRight="10px"
+                  />
+                  <Text as="span" fontSize="md">
+                    {chat.customer}
+                  </Text>
+                </Flex>
+                <Flex marginBottom="20px">
+                  <Avatar
+                      size="sm"
+                      name="Bot Avatar"
+                      src="https://static.vecteezy.com/system/resources/previews/010/054/157/original/chat-bot-robot-avatar-in-circle-round-shape-isolated-on-white-background-stock-illustration-ai-technology-futuristic-helper-communication-concept-in-flat-style-vector.jpg"
+                      marginRight="10px"
+                  />
+                  <Text as="span"> {chat.agent}</Text>
+                </Flex>
+              </Box>
+          ))}
+        </Box>
+        <Spacer />
+        <Box height="10vh" padding="10px">
+          <Flex direction="row">
+            <Input
+                placeholder="Enter your message"
+                onChange={(e) => setMessage(e.target.value)}
+                marginRight="20px"
+                value={message}
+            />
+            <Button colorScheme="blue" size="md" onClick={() => sendMessage(message)}>
+              Send
+            </Button>
+            <Button colorScheme="blue" size="md" onClick={() => sendMessage("tell me about my personality")}>
+              Personality Check
+            </Button>
+            <Spacer />
+            <Spacer />
+            <Spacer />
+          </Flex>
+        </Box>
       </Box>
-      <Box height="10vh" padding="10px">
-        <Flex direction="row">
-          <Input
-            placeholder="Enter your message"
-            onChange={(e) => setMessage(e.target.value)}
-            marginRight="20px"
-            value={message}
-          />
-          <Button colorScheme="red" size="md" onClick={() => sendMessage()}>
-            Send
-          </Button>
-        </Flex>
-      </Box>
-    </Box>
   );
 };
 
